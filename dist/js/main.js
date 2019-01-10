38fb9d3a3a5595a8c8987c4c4d0900bfab6d7ba9
Chris Silich <chris.silich@gmail.com>
2019-01-10 11:20:09 -0500

    changed readme, cleaned up

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-"use strict";window.addEventListener("scroll",function(s){200<window.scrollY?document.body.classList.add("sticky_nav"):document.body.classList.remove("sticky_nav")});
+"use strict";
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 
+// Pseudocode!
 
-window.addEventListener('scroll', function(event) {
-    if (window.scrollY > 200) {
-        document.body.classList.add('sticky_nav')
-    } else {
-        document.body.classList.remove('sticky_nav')
-    }
-})
+// when the user scrolls...
+    // check to see how far they've scrolled (hint: window.scrollY is a variable the browser provides for this)
+    // if they have scrolled down more than 200 pixels,
+        // add the .stick_nav class to the body
+    // or if they haven't scrolled down 200 pixels
+        // remove the .sticky_nav class from the body
